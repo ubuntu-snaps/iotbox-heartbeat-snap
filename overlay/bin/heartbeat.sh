43215fd2f449b061355972f84a93bcdbdc50dcb8
lvming <lvming@restartech.com>
2025-08-17 15:56:58 +0800

    feat: add more message fields

--- overlay/bin/heartbeat.sh
+++ overlay/bin/heartbeat.sh
@@ -2,7 +2,12 @@
 export NATS_URL=$(snapctl get nats.url)
 NATS=/snap/bin/nats-io.nats
 while true; do
-  BODY={\"timestamp\":\"$(date -Iseconds)\",\"hostname\":\"$(hostname)\",\"machine_id\":\"$(cat /etc/machine-id)\"}
-  $NATS pub iotbox.heartbeat.$(cat /etc/machine-id) $BODY
+  TS=$(date -Iseconds)
+  MACHINE_ID=$(cat /etc/machine-id)
+  HOSTNAME=$(hostname)
+  FQDN=$(hostname -f)
+  ADDRESSES=$(hostname -I)
+  MSG={\"timestamp\":\"$TS\",\"machine_id\":\"$MACHINE_ID\",\"hostname\":\"$HOSTNAME\",\"fqdn\":\"$FQDN\",\"addresses\":\"$ADDRESSES\"}
+  $NATS pub iotbox.heartbeat.$(cat /etc/machine-id) $MSG
   sleep 10
 done
