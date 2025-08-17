67b97824ee25a6e6fd38560fd182a46313b4def4
lvming <lvming@restartech.com>
2025-08-17 16:01:18 +0800

    feat: add natscli

--- overlay/bin/heartbeat.sh
+++ overlay/bin/heartbeat.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 export NATS_URL=$(snapctl get nats.url)
-NATS=/snap/bin/nats-io.nats
 while true; do
   TS=$(date -Iseconds)
   MACHINE_ID=$(cat /etc/machine-id)
@@ -8,6 +7,6 @@ while true; do
   FQDN=$(hostname -f)
   ADDRESSES=$(hostname -I)
   MSG={\"timestamp\":\"$TS\",\"machine_id\":\"$MACHINE_ID\",\"hostname\":\"$HOSTNAME\",\"fqdn\":\"$FQDN\",\"addresses\":\"$ADDRESSES\"}
-  $NATS pub iotbox.heartbeat.$(cat /etc/machine-id) $MSG
+  nats pub iotbox.heartbeat.$MACHINE_ID $MSG
   sleep 10
 done
